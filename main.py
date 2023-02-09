f3b8096fe9c1cc8cfb0899eca10b395e0d9a7563
Ana María <if720567@iteso.mx>
2023-02-08 22:37:37 -0600

    actualizar functions
    actualizar functions

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ Created on Fri Jan 20 08:17:04 2023
 @author: HP
 """
 
-import functions as fn
-from data import df
-
-#iloc[fila,columna] (texto)
-#loc[fila,coliumna] (número > posición)
-df.loc[:,'mean_1'] = fn.remove_mean(df.iloc[:,0])
-df.loc[:,'add_value'] = fn.add_number(df.iloc[:,0], 3)
+"""
+# -- --------------------------------------------------------------------------------------------------- -- #
+# -- project: Laboratorio 1: Inversión de Capital                                                         -- #
+# -- script: functions.py : python script with general functions                                         -- #
+# -- author: anamaria1404 y Galileatr                                                                       -- #
+# -- license: GPL-3.0 License                                                                            -- #
+# -- repository: https://github.com/anamaria1404/Laboratorio-I                                                                     -- #
+# -- --------------------------------------------------------------------------------------------------- -- #
+"""
 
