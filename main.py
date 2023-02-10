150c4912d51024c04cf0e26292987bc2523ccae5
Ana María <if720567@iteso.mx>
2023-02-10 08:36:22 -0600

    Limpiar tickers
    Limpiar tickers

--- main.py
+++ main.py
@@ -11,15 +11,26 @@
 # -- --------------------------------------------------------------------------------------------------- -- #
 """
 
+import data
+
+# obtener tickers repetidos
+cant = vertical_concat.groupby(['Ticker'])['Ticker'].count().to_frame()
+cant24 = cant[cant['Ticker'] == 25] #tickers repetidos 25 veces
+
+#pesos del mes 1
+pesos1 = df[['Ticker','Peso (%)']]
+pesos1 = pesos1.loc[pesos1['Ticker'].isin((cant24.index))].set_index('Ticker')
+pesos1 = pesos1["Peso (%)"] = df["Peso (%)"] / 100
+pesos1["Ticker"] = [pesos1["Ticker"].replace(".", "-").replace("*", "") + ".MX" for i in list(pesos1["Ticker"])
+
+
 def passive(naftrac_date = 20210129, capital = 1000000, comission = 0.00125):
     
     df, naftrac_date = data.file_reading(naftrac_date = naftrac_date)
     df = data.data_wrangling(df)
     fig1, fig2 = visualizations.pie_chart(df)
     
-    df["Peso (%)"] = df["Peso (%)"] / 100
-    [df.drop(labels = i, axis = 0, inplace = True) for i in range(len(df)) if df["Ticker"][i] in ["KOFL", "KOFUBL", "MXN", "BSMXB", "NMKA"]];
-    df.reset_index(drop = True, inplace = True)
+
     df["Ticker"] = [stock.replace(".", "-").replace("*", "") + ".MX" for stock in list(df["Ticker"])]
     
     df_passive, fig3, df_metrics = functions.passive_investment_strategy(df, naftrac_date, capital, comission, 
