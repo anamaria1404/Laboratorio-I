f42de9767dea48446aade9c78e9ade721d2b734c
Ana María <if720567@iteso.mx>
2023-02-10 08:39:12 -0600

    Actualización
    Actualización

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ cant24 = cant[cant['Ticker'] == 25] #tickers repetidos 25 veces
 #pesos del mes 1
 pesos1 = df[['Ticker','Peso (%)']]
 pesos1 = pesos1.loc[pesos1['Ticker'].isin((cant24.index))].set_index('Ticker')
-pesos1 = pesos1["Peso (%)"] = df["Peso (%)"] / 100
+pesos1["Peso (%)"] = df["Peso (%)"] / 100
 pesos1["Ticker"] = [pesos1["Ticker"].replace(".", "-").replace("*", "") + ".MX" for i in list(pesos1["Ticker"])
 
 
